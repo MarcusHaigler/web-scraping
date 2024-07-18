50759c8c5c9d8500b92f09be489a9eee73678299
MarcusHaigler <haiglerm41@gmail.com>
2024-07-18 19:49:36 -0400

    testing the test

--- web_scraper.py
+++ web_scraper.py
@@ -1,21 +1,26 @@
 from urllib.request import urlopen
-from google import search
+from googlesearch import search
 
-def page_html(url):
+def parse_html(url):
    # read and decode page into html
    page = urlopen(url)
    html_bytes = page.read()
    html = html_bytes.decode("utf-8")
    
+   print("html parsed...")
    return html
 
 def google_search_results(query, pages):
    pages = []
-   for url in search(query):
+   for url in search(query, stop=3):
       pages.append(url)
 
-   print(pages)
+   print("pages loaded...")
    return pages
+   
+def read_robots(url): # check robot.txt
+   pass
+   
    
 def scrape_page(page):
    # scrapes page html for metadata
@@ -25,5 +30,7 @@ urls = []
 search_query = input("enter search query: ")
 
 google_search_results(search_query, urls)# gather top results
-page_html(urls)# read html
-scrape_page()
+### for website in urls:
+parse_html(urls)# read html
+   
+# scrape_page()
