ba64979b8c9a33b72bdd6eab2c82dee02db15b01
MarcusHaigler <haiglerm41@gmail.com>
2024-07-19 19:46:44 -0400

    web scraping stuff:

--- web_scraper.py
+++ web_scraper.py
@@ -1,36 +1,36 @@
 from urllib.request import urlopen
 from googlesearch import search
+import urllib.robotparser as urobot
+import urllib.request
+from bs4 import BeautifulSoup
+import os
 
-def parse_html(url):
-   # read and decode page into html
-   page = urlopen(url)
-   html_bytes = page.read()
-   html = html_bytes.decode("utf-8")
-   
-   print("html parsed...")
-   return html
+def google_search(query):
+   for u in search(str(query), num_results=5):
+      os.system('cls||clear')
+      print(u)
+      urls.append(u)
+      print(f"progress: {len(urls)} / 5")
+      if len(urls) == 5:
+         print("all urls fetched")
 
-def google_search_results(query, pages):
-   pages = []
-   for url in search(query, stop=3):
-      pages.append(url)
-
-   print("pages loaded...")
-   return pages
-   
-def read_robots(url): # check robot.txt
-   pass
-   
-   
-def scrape_page(page):
-   # scrapes page html for metadata
-   pass
-
+def check_robots(url): # check robots.txt
+   global url_robot
+   global l
+   for i in url: # read trhu url and find the end of the domain
+      if url.index(i, i+3) != ".com":
+         pass
+      else:
+         l = url.index(0,i+3)
+         url_robot = url.index(0,i+3) # save as variable
+         print(f"robots.txt found at {url_robot}")
+   # read txt file and find look for the value    
+      
 urls = []
-search_query = input("enter search query: ")
+search_query = "cats" # make this an input for final version
 
-google_search_results(search_query, urls)# gather top results
-### for website in urls:
-parse_html(urls)# read html
-   
-# scrape_page()
+google_search(search_query)
+print(urls) # for testing
+
+for url in urls:
+   check_robots(url)
