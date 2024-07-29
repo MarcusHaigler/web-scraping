486f0d15224770749b5c60520b026127a8dd4fad
MarcusHaigler <haiglerm41@gmail.com>
2024-07-29 02:20:57 -0400

    fixed check_robots.py, need to adjust it to take in entire lists

--- check_robots.py
+++ check_robots.py
@@ -7,8 +7,8 @@ import os
 
 sample_urls = ['https://www.imdb.com/title/tt5697572/', 'https://www.reddit.com/r/cats/', 'https://www.imdb.com/title/tt5697572/', 'https://www.imdb.com/title/tt5697572/', 'https://www.imdb.com/title/tt5697572/']
 
-def get_home_page(word, end_value):
-  hp = word.index(12,word.len()) # cut off the 'https://www.'
+def get_home_page(word):
+  hp = word[12:] # cut off the 'https://www.'
   end_value = '/' # the end of the url name
   captured_url = ""
 # loop thru the rest of the url and find the end value
@@ -17,34 +17,39 @@ def get_home_page(word, end_value):
     if i != end_value:
       pass
     else:
-      return captured_url = hp.index(0,i)
+      captured_url = hp[:i]
+
+      return captured_url
     
-    return "could not find end of url"
+    return False
     
-def check_robots(url): # check robots.txt
+def check_robots(url_list): # check robots.txt for permission
    os.system("cls||clear")
+   
    print(f'checking robots for {url}')
    robot_url = ""
    can_fetch_status = False
   
 # integrate get_home_page here to find the end of the url homepage
-  if get_home_page(url) is False:
+   if get_home_page(url) is False:
     print("url capture failed")
-  else:
+    
+   else:
+    print("url capture successful")
     robot_url = get_home_page(url)
     
-# read txt file for general rules   
-   urlbot = urllib.robotparser.RobotFileParser()
-   urlbot.set_url(robot_url)
-   urlbot.read()
-   if urlbot.can_fetch("*", url):
-      can_fetch_status =  True
-   else:
-      can_fetch_status =  False
-   
-   return can_fetch_status
+    # read txt file for general rules   
+    urlbot = urllib.robotparser.RobotFileParser()
+    urlbot.set_url(robot_url)
+    urlbot.read()
+    if urlbot.can_fetch("*", url):
+        can_fetch_status =  True
+    else:
+        can_fetch_status =  False
+    
+    return can_fetch_status
 
-for url in sample_urls:
+for url in sample_urls: # remove all websites that dont allow web scraping
    if check_robots(url) is True:
       pass
    else:
